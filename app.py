baaa2fc084969f241bb8e751e70c2a9aa1f90a49
jurchiks33 <jurisslisans@gmail.com>
2024-04-06 18:48:11 +0200

    py updates

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Placeholder data for the templates
+    featured_recipes = [{'name': 'Tomato Soup', 'description': 'A delicious tomato soup recipe.'},]
+    latest_posts = [{'title': 'The Art of Baking', 'summary': 'Learn the secrets of perfect baking with our latest guide.'},]
+    testimonials = [{'user': 'John Doe', 'comment': 'This app transformed my cooking!'}]
+    
+    return render_template('index.html', 
+                           featured_recipes=featured_recipes,
+                           latest_posts=latest_posts,
+                           testimonials=testimonials)
 
 if __name__ == '__main__':
     app.run(debug=True)
