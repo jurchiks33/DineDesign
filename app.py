b0b61286eb797407b753487d2eddd7084737c4d7
jurchiks33 <jurisslisans@gmail.com>
2024-04-06 19:02:46 +0200

    fetching test recepie

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Placeholder data for the templates
+    # Placeholder data for the home page templates
     featured_recipes = [{'name': 'Tomato Soup', 'description': 'A delicious tomato soup recipe.'},]
     latest_posts = [{'title': 'The Art of Baking', 'summary': 'Learn the secrets of perfect baking with our latest guide.'},]
     testimonials = [{'user': 'John Doe', 'comment': 'This app transformed my cooking!'}]
     
-    return render_template('index.html', 
+    # Here we fetch more recipes from a database.
+    # For now, using placeholder data
+
+    return render_template('index.html',
                            featured_recipes=featured_recipes,
                            latest_posts=latest_posts,
                            testimonials=testimonials)
-
-if __name__ == '__main__':
-    app.run(debug=True)
